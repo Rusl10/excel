6f7befc237056ba55262f708c7ee09559540638d
Rusl10 <cloud_47@mail.ru>
2020-11-27 10:42:07 +0300

    base of store created

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import {Toolbar} from '@/components/toolbar/Toolbar'
 import {Formula} from '@/components/formula/Formula'
 import {Table} from '@/components/table/Table'
 import {Excel} from '@/components/excel/Excel'
+import {rootReducer} from '@/redux/rootReducer'
+import {createStore} from '@/core/createStore'
+import {initialState} from '@/redux/initialState'
+
+debugger
+const store = createStore(rootReducer, initialState)
+console.log('store', store)
+window.store = store
 
 const excel = new Excel('#app', {
 	components: [
---
+++ src/redux/initialState.js
@@ -0,0 +1,42 @@
+import {defaultStyles, defaultTitle} from '@/constants'
+import {storage} from '@/core/utils'
+debugger
+const defaultState = {
+	colState: {},
+	rowState: {},
+	dataState: {}, // {'0:1': 'adadadafvs'},
+	currentText: '',
+	// Стили из тулбара для выбранных ячеек по id
+	stylesState: {},
+	/*текущий Объект стилей тулбара {
+	textAlign: 'left',
+	fontWeight: 'normal',
+	fontStyle: 'normal',
+	textDecoration: 'none'
+	} к примеру*/
+	currentStyles: defaultStyles,
+	title: defaultTitle
+
+}
+
+const normalize = state => ({
+	...state,
+	currentStyles: defaultStyles,
+	currentText: '',
+	// headerState: ''
+})
+
+
+export const initialState = normalize(storage('excel-state')) ? storage('excel-state') : pushDefaultStateToLocalStorage()
+console.log('initialState', initialState)
+function pushDefaultStateToLocalStorage(){
+	storage('excel-state', defaultState)
+	return defaultState
+}
+
+// export const initialState = normalize(storage('excel-state')) ? storage('excel-state') : pushDefaultStateToLocalStorage()
+
+// function pushDefaultStateToLocalStorage(){
+// 	storage('excel-state', defaultState)
+// 	return defaultState
+// }
